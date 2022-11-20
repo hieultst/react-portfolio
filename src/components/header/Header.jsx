bfaf3d188ad85b066bec0300595285cd80723506
Hieult <infor.letrunghieu@gmail.com>
2022-11-20 16:15:41 +0700

    Nav - floating nav component css - managing nav states using useState Hook

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <header>
             <div className="container header__container">
-                <h5>Hello, I'm</h5>
+                <h5>Hello, I'm </h5>
                 <h1>Le Trung Hieu</h1>
                 <h5 className="text-light">Web Developer</h5>
                 <CTA />
