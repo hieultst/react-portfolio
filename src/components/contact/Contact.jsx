99005b5db287b6ce4e5fa43b9498de4bb16c9811
Hieult <infor.letrunghieu@gmail.com>
2022-12-23 21:12:44 +0700

    Form submission using EmailJS

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
+
 import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiMessengerLine } from "react-icons/ri";
 import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm("service_hmyd5dg", "template_s2wvxv8", form.current, "pQIOzIRMgAD5b-Sux");
+
+        e.target.reset();
+    };
+
     return (
         <section id="contact">
             <h5>Get In Touch</h5>
@@ -38,7 +49,7 @@ const Contact = () => {
                     </article>
                 </div>
                 {/* END OF CONTACT OPTIONS */}
-                <form action="">
+                <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name="name" placeholder="Your Full Name" required />
                     <input type="email" name="email" placeholder="Your Email" required />
                     <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
